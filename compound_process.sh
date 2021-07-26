2ee05548ec5f178b80b688521d5e5ac2cd43e356
Kendall J Comeaux <kcome26@lsu.edu>
2021-07-26 12:52:12 -0500

    change unzip command order

--- compound_process.sh
+++ compound_process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-unzip -d  working input/*
+unzip input/* -d  working
 python3 naming_pattern_checks.py
 if [ $? -eq 0 ] 
 then
@@ -7,9 +7,9 @@ then
 	if [ $? -eq 0 ] 
 	then
 		#cd /var/www/drupal7/sites/all/modules/islandora_compound_batch/extras/scripts
-		cd /home/wwc/Clones/islandora_compound_batch/extras/scripts
+		cd /Users/kcome26/islandora_compound_batch/extras/scripts
 		#php create_structure_files.php /etc/islandora-auto-ingest/output/
-		php create_structure_files.php /home/wwc/Clones/islandora-auto-ingest-kit/output
+		php create_structure_files.php /Users/kcome26/islandora-auto-ingest-kit/output
 		echo 'packaged and structured'
 	fi
 else
--- simple_process.sh
+++ simple_process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-unzip -d working input/*
+unzip input/* -d working
 python3 naming_pattern_checks.py
 mv working/* output/
 python3 simple_drush_cmd.py
