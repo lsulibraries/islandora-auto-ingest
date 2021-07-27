d28cb7c90e061b5c1914013b0ccfe18a9cdd9930
kendall <kcome26@lsu.edu>
2021-07-27 11:10:27 -0700

    paths updated for fossa

--- compound_process.sh
+++ compound_process.sh
@@ -7,13 +7,13 @@ then
 	if [ $? -eq 0 ] 
 	then
 		#cd /var/www/drupal7/sites/all/modules/islandora_compound_batch/extras/scripts
-		cd /home/wwc/Clones/islandora_compound_batch/extras/scripts
+		cd /home/fossa/islandora_compound_batch/extras/scripts
 		#php create_structure_files.php /etc/islandora-auto-ingest/output/
-		php create_structure_files.php /home/wwc/Clones/islandora-auto-ingest/output
+		php create_structure_files.php /home/fossa/islandora-auto-ingest/output
 		echo 'packaged and structured'
 	fi
 else
 	echo 'check files for naming patterns, packaging canceled'
 fi
-cd /home/wwc/Clones/islandora-auto-ingest
+cd /home/fossa/islandora-auto-ingest
 python3 compound_drush_cmd.py
