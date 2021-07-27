47806e7d12739249830b43ffd0c909ffe9ffec77
kcome26 <87440778+kcome26@users.noreply.github.com>
2021-07-27 12:10:11 -0500

    remove kit

--- compound_process.sh
+++ compound_process.sh
@@ -9,7 +9,7 @@ then
 		#cd /var/www/drupal7/sites/all/modules/islandora_compound_batch/extras/scripts
 		cd /home/wwc/Clones/islandora_compound_batch/extras/scripts
 		#php create_structure_files.php /etc/islandora-auto-ingest/output/
-		php create_structure_files.php /home/wwc/Clones/islandora-auto-ingest-kit/output
+		php create_structure_files.php /home/wwc/Clones/islandora-auto-ingest/output
 		echo 'packaged and structured'
 	fi
 else
