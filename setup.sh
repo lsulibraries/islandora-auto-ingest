8ac78ed2b944af66dbf8eccab9a7c550c035fe0a
kendall <kcome26@lsu.edu>
2021-07-27 10:05:44 -0700

    add steps to setup

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 mkdir input
 mkdir working
 mkdir output
+sudo apt install php-xml
+sudo apt install php7.4-cli
+sudo apt install git
+git clone https://github.com/lsulibraries/islandora_compound_batch
+git clone https://github.com/lsulibraries/islandora-auto-ingest
